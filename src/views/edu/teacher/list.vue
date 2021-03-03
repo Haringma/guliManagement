3c767364e15198c63cf0abeae517ea8e3320c233
mmycode <845443900@qq.com>
2021-03-03 14:15:06 +0800

    讲师列表页面

--- src/views/edu/teacher/list.vue
+++ src/views/edu/teacher/list.vue
@@ -1,6 +1,47 @@
 <template>
   <div class="app-container">
-    讲师表单
+        <!-- 表格 -->
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      element-loading-text="数据加载中"
+      border
+      fit
+      highlight-current-row>
+
+      <el-table-column
+        label="序号"
+        width="70"
+        align="center">
+        <template slot-scope="scope">
+          {{ (page - 1) * limit + scope.$index + 1 }}
+        </template>
+      </el-table-column>
+
+      <el-table-column prop="name" label="名称" width="80" />
+
+      <el-table-column label="头衔" width="80">
+        <template slot-scope="scope">
+          {{ scope.row.level===1?'高级讲师':'首席讲师' }}
+        </template>
+      </el-table-column>
+
+      <el-table-column prop="intro" label="资历" />
+
+      <el-table-column prop="gmtCreate" label="添加时间" width="160"/>
+
+      <el-table-column prop="sort" label="排序" width="60" />
+
+      <el-table-column label="操作" width="200" align="center">
+        <template slot-scope="scope">
+          <router-link :to="'/edu/teacher/edit/'+scope.row.id">
+            <el-button type="primary" size="mini" icon="el-icon-edit">修改</el-button>
+          </router-link>
+          <el-button type="danger" size="mini" icon="el-icon-delete" @click="removeDataById(scope.row.id)">删除</el-button>
+        </template>
+      </el-table-column>
+      
+    </el-table>
   </div>
 </template>
 
@@ -10,7 +51,33 @@ import teacher from '@/api/edu/teacher'
 
 export default {
     //写核心代码的位置
-
+    // data:{
+    // },
+    data(){//定义变量和初始值
+      return{
+        list:null,//查询之后接口返回集合
+        page:1,//当前页
+        limit:10,//每页记录数
+        teacherQuery:{//条件封装对象
+        }
+      }
+    },
+    created(){//页面渲染之前执行，一般调用method中的方法
+        this.getList()
+    },
+    methods:{//创建具体的方法，调用teacher.js定义的方法
+      getList(){
+        teacher.getTeacherListPage(this.page,this.limit,this.teacherQuery)
+              .then(response =>{//请求成功
+                  //response接口返回的数据
+                  console.log(response)
+                  this.list = response.data.rows
+              })
+              .catch(error => {//请求失败
+                console.error();
+              })
+      }
+    }
 
 }
 </script>
